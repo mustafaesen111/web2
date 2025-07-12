1fe07ecfc6836aa18926078b2c45f45aff9e899e
mustafaesen111 <78534067+mustafaesen111@users.noreply.github.com>
2025-07-12 16:55:46 +0600

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-    
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    password = db.Column(db.String(200), nullable=False)
+    is_subscribed = db.Column(db.Boolean, default=False)  # Abonelik durumu
